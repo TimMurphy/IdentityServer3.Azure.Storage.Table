ef23b55b3aebbe0d30c3d1de0d84a041e81f9446
Tim Murphy <tim@26tp.com>
2016-05-12 18:29:50 +1000

    Add IUserService "template" code from IdentityServer3 InMemoryUserService

---
+++ source/IdentityServer3.Azure.Storage.Table/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer3.Azure.Storage.Table.Models
+{
+    /// <summary>
+    ///     User model used by 
+    ///     <see cref="IdentityServer3.Azure.Storage.Table.Services.User"/>
+    /// </summary>
+    /// <remarks>
+    ///     Copied from <see cref="IdentityServer3.Core.Services.InMemory.InMemoryUser"/>
+    /// </remarks>
+    public class User
+    {
+        public User()
+        {
+            Enabled = true;
+            Claims = new List<Claim>();
+        }
+
+        public string Subject { get; set; }
+        public bool Enabled { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Provider { get; set; }
+        public string ProviderId { get; set; }
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+}
--- source/IdentityServer3.Azure.Storage.Table/Services/UserService.cs
+++ source/IdentityServer3.Azure.Storage.Table/Services/UserService.cs
@@ -1,50 +1,155 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityServer3.Azure.Storage.Table.Models;
+using IdentityServer3.Core.Extensions;
 using IdentityServer3.Core.Models;
 using IdentityServer3.Core.Services;
+using IdentityServer3.Core.Services.Default;
 
 namespace IdentityServer3.Azure.Storage.Table.Services
 {
-    public class UserService : IUserService
+    /// <summary>
+    ///     Azure Storage Table persistence layer for <see cref="User"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Copied from <see cref="IdentityServer3.Core.Services..UserService"/>.
+    /// </remarks>
+    public class UserService : UserServiceBase
     {
-        public UserService()
+        /// <summary>
+        ///     This methods gets called for local authentication (whenever 
+        ///     the user uses the username and password dialog).
+        /// </summary>
+        public override Task AuthenticateLocalAsync(LocalAuthenticationContext context)
         {
-            throw new NotImplementedException("todo");
+            //var query =
+            //    from u in _users
+            //    where u.Username == context.UserName && u.Password == context.Password
+            //    select u;
+
+            //var user = query.SingleOrDefault();
+            //if (user != null)
+            //{
+            //    context.AuthenticateResult = new AuthenticateResult(user.Subject, GetDisplayName(user));
+            //}
+
+            //return Task.FromResult(0);
+            throw new NotImplementedException();
         }
 
-        public Task PreAuthenticateAsync(PreAuthenticationContext context)
+        /// <summary>
+        ///     This method gets called when the user uses an external identity 
+        ///     provider to authenticate.
+        /// </summary>
+        public override Task AuthenticateExternalAsync(ExternalAuthenticationContext context)
         {
-            throw new NotImplementedException("todo");
+            //var query =
+            //    from u in _users
+            //    where
+            //        u.Provider == context.ExternalIdentity.Provider &&
+            //        u.ProviderId == context.ExternalIdentity.ProviderId
+            //    select u;
+
+            //var user = query.SingleOrDefault();
+            //if (user == null)
+            //{
+            //    string displayName;
+
+            //    var name = context.ExternalIdentity.Claims.FirstOrDefault(x => x.Type == Constants.ClaimTypes.Name);
+            //    if (name == null)
+            //    {
+            //        displayName = context.ExternalIdentity.ProviderId;
+            //    }
+            //    else
+            //    {
+            //        displayName = name.Value;
+            //    }
+
+            //    user = new User
+            //    {
+            //        Subject = CryptoRandom.CreateUniqueId(),
+            //        Provider = context.ExternalIdentity.Provider,
+            //        ProviderId = context.ExternalIdentity.ProviderId,
+            //        Username = displayName,
+            //        Claims = context.ExternalIdentity.Claims
+            //    };
+            //    _users.Add(user);
+            //}
+
+            //context.AuthenticateResult = new AuthenticateResult(user.Subject, GetDisplayName(user), identityProvider: context.ExternalIdentity.Provider);
+
+            //return Task.FromResult(0);
+            throw new NotImplementedException();
         }
 
-        public Task AuthenticateLocalAsync(LocalAuthenticationContext context)
+        /// <summary>
+        ///     This method is called whenever claims about the user are requested 
+        ///     (e.g. during token creation or via the userinfo endpoint)
+        /// </summary>
+        public override Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            throw new NotImplementedException("todo");
+            //var query =
+            //    from u in _users
+            //    where u.Subject == context.Subject.GetSubjectId()
+            //    select u;
+            //var user = query.Single();
+
+            //var claims = new List<Claim>{
+            //    new Claim(Constants.ClaimTypes.Subject, user.Subject),
+            //};
+
+            //claims.AddRange(user.Claims);
+            //if (!context.AllClaimsRequested)
+            //{
+            //    claims = claims.Where(x => context.RequestedClaimTypes.Contains(x.Type)).ToList();
+            //}
+
+            //context.IssuedClaims = claims;
+
+            //return Task.FromResult(0);
+            throw new NotImplementedException();
         }
 
-        public Task AuthenticateExternalAsync(ExternalAuthenticationContext context)
+        /// <summary>
+        ///     This method gets called whenever identity server needs to determine 
+        ///     if the user is valid or active (e.g. during token issuance or 
+        ///     validation)
+        /// </summary>
+        /// <exception cref="System.ArgumentNullException">subject</exception>
+        public override Task IsActiveAsync(IsActiveContext context)
         {
-            throw new NotImplementedException("todo");
+            //if (context.Subject == null) throw new ArgumentNullException("subject");
+
+            //var query =
+            //    from u in _users
+            //    where
+            //        u.Subject == context.Subject.GetSubjectId()
+            //    select u;
+
+            //var user = query.SingleOrDefault();
+
+            //context.IsActive = (user != null) && user.Enabled;
+
+            //return Task.FromResult(0);
+            throw new NotImplementedException();
         }
 
-        public Task PostAuthenticateAsync(PostAuthenticationContext context)
+        /// <summary>
+        ///     Retrieves the display name.
+        /// </summary>
+        protected virtual string GetDisplayName(User user)
         {
-            throw new NotImplementedException("todo");
-        }
+            //var nameClaim = user.Claims.FirstOrDefault(x => x.Type == Constants.ClaimTypes.Name);
+            //if (nameClaim != null)
+            //{
+            //    return nameClaim.Value;
+            //}
 
-        public Task SignOutAsync(SignOutContext context)
-        {
-            throw new NotImplementedException("todo");
-        }
-
-        public Task GetProfileDataAsync(ProfileDataRequestContext context)
-        {
-            throw new NotImplementedException("todo");
-        }
-
-        public Task IsActiveAsync(IsActiveContext context)
-        {
-            throw new NotImplementedException("todo");
+            //return user.Username;
+            throw new NotImplementedException();
         }
     }
 }
